19279d2781f0cfa9328ead01f736a7086d71ac04
Leire Gonzalez <lhocasar@gmail.com>
2015-06-07 10:57:10 +0200

    random2 version 2

--- random2.js
+++ random2.js
@@ -1,9 +1,9 @@
 //Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
-if (numero <= 0.5){
-  console.log('\n' + numero + ' MENOR que 0,5 \n');
-}
-else{
-  console.log('\n' + numero + ' MAYOR que 0,5 \n');
-}
+var mensaje = ' MAYOR que 0,5 \n';
+
+if (numero <= 0.5)
+  mensaje =' MENOR que 0,5 \n';
+
+ console.log('\n' + numero + mensaje);
